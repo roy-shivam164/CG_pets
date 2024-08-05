b17af4fd5ed7a72610ec6919ac294deed2494151
roy-shivam164 <shivam.rai@cginfinity.com>
2024-08-05 16:07:04 +0530

    updated fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 // src/components/Header.jsx
 const Header = ({ openModal }) => (
-    <header className="bg-blue-500 p-4">
+    <header className=" p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-3xl font-bold">CG Pets</div>
+        <div className="text-blue-600 text-5xl font-bold">CG Pets</div>
         <nav className="space-x-4">
-          <a href="#home" className="text-white hover:text-gray-200">Home</a>
-          <a href="#products" className="text-white hover:text-gray-200">Products</a>
-          <a href="#about" className="text-white hover:text-gray-200">About Us</a>
-          <a href="#contact" className="text-white hover:text-gray-200">Contact</a>
-          <button onClick={openModal} className="bg-white text-blue-600 hover:bg-gray-100 font-bold py-2 px-4 rounded">Sign Up</button>
+          <a href="#home" className="text-blue-600 hover:font-bold">Home</a>
+          <a href="#products" className="text-blue-600 hover:font-bold">Products</a>
+          <a href="#about" className="text-blue-600 hover:font-bold">About Us</a>
+          <a href="#contact" className="text-blue-600 hover:font-bold">Contact</a>
+          <button onClick={openModal} className="bg-white text-blue-600 hover:bg-blue-400 hover:text-white font-bold py-2 px-4 rounded">Sign Up</button>
         </nav>
       </div>
     </header>
